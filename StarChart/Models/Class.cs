be2d2cda9cf8ab8fcdafb63ddefb393a8ac529f8
Chaithra-Git <64492736+Chaithra-Git@users.noreply.github.com>
2020-10-31 19:24:44 -0700

    Add and configure Middleware

---
+++ StarChart/Models/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id;
+        [RequiredAttribute]
+        public string Name;
+        public int? OrbitedObjectId;
+        [NotMapped]
+        public List<CelestialObject> Satellites;
+        public TimeSpan OrbitalPeriod;
+
+    }
+}
