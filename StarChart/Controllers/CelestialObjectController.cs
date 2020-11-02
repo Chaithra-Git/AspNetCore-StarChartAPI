00bd2bc9fcf6e69a86dfcb98023c369b20c87a70
Chaithra-Git <64492736+Chaithra-Git@users.noreply.github.com>
2020-11-01 16:05:22 -0800

    Create get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -9,13 +9,35 @@ namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
-    public class CelestialObjectController:ControllerBase
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+       // [HttpGet("{id:int}", Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+
+            // var celestialObj = _context.CelestialObjects.Where(c => c.Id == id);
+            var celestialObj = _context.CelestialObjects.Where(c => c.Id == id)
+
+
+            if (celestialObj == null)
+            {
+                return NotFound();
+            }
+
+        
+
+            else
+            {
+                return Ok(celestialObj);
+            }
+
         }
 
     }
